7d7489957b7e8c282731ba65d212613ab5334cc5
Aluno <ricardomulersevero@gmail.com>
2023-05-09 17:24:44 -0300

    Aula 13 do Curso

---
+++ exercicios/formjava/main.js
@@ -0,0 +1,8 @@
+addEventListener('submit', function(e){
+    parseInt(num1);
+    if(num1 > num2){
+        return alert("Primeiro é maior");
+    }else if(num2 > num1){
+        return alert("Segundo é maior");
+    }
+})
