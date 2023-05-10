507bfa814fe4f3364e24c86edfa22513b375cd20
Aluno <ricardomulersevero@gmail.com>
2023-05-10 14:46:45 -0300

    Aula 14 do Curso

--- exercicios/formjava/main.js
+++ exercicios/formjava/main.js
@@ -1,26 +1,17 @@
-const num1 = document.getElementById('num1')
-const num2 = document.getElementById('num2')
 
 console.log(num1.value);
 console.log(num2.value);
 
 addEventListener('submit', function(e){
 
-<<<<<<< HEAD
-=======
-    let num1 = parseInt(document.getElementById("numero1").value);
-    let num2 = parseInt(document.getElementById("numero2").value);
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
 
->>>>>>> 79c8b9a6152863c100d2d56cb0ca7e84e7352dd7
     if(num1 > num2){
         alert("Primeiro é maior");
     }else if(num2 > num1){
-<<<<<<< HEAD
         return alert("Segundo é maior");
     }else{
         return alert("São iguais")
-=======
-        alert("Segundo é maior");
->>>>>>> 79c8b9a6152863c100d2d56cb0ca7e84e7352dd7
     }
 })
