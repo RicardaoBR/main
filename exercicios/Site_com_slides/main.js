abe9eaf2b8424b3643319a9a01f867f1bd892eb2
Aluno <ricardomulersevero@gmail.com>
2023-05-23 17:16:29 -0300

    Aula do Curso 23/05/23

--- exercicios/Site_com_slides/main.js
+++ exercicios/Site_com_slides/main.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
         autoplay: true
     })
 
-    $('.menu_hamburger').click(function(){
+    $('.menu_hamburger').click(function () {
         $('nav').slideToggle();
     })
+
+    $('#telefone').mask('(00)00000-0000', {
+        placeholder: '(12)34567-8901'
+    })
 })
+
