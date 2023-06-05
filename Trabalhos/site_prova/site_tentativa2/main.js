27025adfa1d20d272f96d06eea352d3446908780
Aluno <ricardomulersevero@gmail.com>
2023-06-05 17:16:32 -0300

    Aula do Curso 05/06/23

--- Trabalhos/site_prova/site_tentativa2/main.js
+++ Trabalhos/site_prova/site_tentativa2/main.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+    $('#botao_menos').click(function (e) {
+        $('#seçoes').slideUp();
+        $('#botao_mais').css("opacity", "1");
+    })
+
+    $('#botao_mais').click('submit', function (e) {
+        $('#seçoes').slideDown();
+        $('#botao_mais').css("opacity", "0");
+    })
+
+    $('#botao_cancelar').click('submit', function (e) {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const endereçoDaNovaImagem = $('#endereço_imagem').val();
+        const novoItem = $('<li style = "display: none;"></li>');
+        $(`<img src= "${endereçoDaNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+                <div class="image_link">
+                    <a href="${endereçoDaNovaImagem}" alt="Tamanho real" target="_blank">
+                        Ver imagem original
+                    </a>
+                </div>
+            `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereço_imagem').val('')
+    })
 
     $('#seçao_pessoal').click(function () {
         $('#seçao_pessoal_pagina').hide(1000);
@@ -56,7 +86,7 @@ $(document).ready(function () {
         $('#seçao_livros_pagina').hide(1000);
     })
 
-    $('#botao_abrir_seçoes').click('submit', function(e){
+    $('#botao_abrir_seçoes').click('submit', function (e) {
         $('seçoes').slideUp();
     })
 
